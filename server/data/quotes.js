7314e3e3bc6206dfff3280325329017b89657781
Elena Bauer <elena.s.bauer@gmail.com>
2026-08-18 12:57:54 +0200

    Creating own quotes API Node.js Express server with resource/quotes/random/single

---
+++ server/data/quotes.js
@@ -0,0 +1,55 @@
+const quotes = [
+  {
+    id: 'c6e91a42',
+    text: 'The secret of getting ahead is getting started',
+    author: 'Mark Twain',
+  },
+  {
+    id: '8b3d57f1',
+    text: 'It does not matter how slowly you go as long as you do not stop',
+    author: 'Confucius',
+  },
+  {
+    id: 'f24a6c89',
+    text: 'Life is what happens when you are busy making other plans',
+    author: 'John Lennon',
+  },
+  {
+    id: '1e7b4d35',
+    text: 'The journey of a thousand miles begins with one step',
+    author: 'Lao Tzu',
+  },
+  {
+    id: 'a92f18c7',
+    text: 'Do what you can, with what you have, where you are',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: '5d4e83ab',
+    text: 'Happiness depends upon ourselves',
+    author: 'Aristotle',
+  },
+  {
+    id: 'b71c29e6',
+    text: 'Everything you can imagine is real',
+    author: 'Pablo Picasso',
+  },
+  {
+    id: '3f86d1a4',
+    text: 'The harder I work, the luckier I get',
+    author: 'Samuel Goldwyn',
+  },
+  {
+    id: 'e58a72c3',
+    text: 'Act as if what you do makes a difference. It does',
+    author: 'William James',
+  },
+  {
+    id: '6c14f9b8',
+    text: 'Great things are done by a series of small things brought together',
+    author: 'Vincent van Gogh',
+  },
+];
+
+
+module.exports = quotes;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const quotes = require('./data/quotes');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+function getRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
+app.get('/quotes/random/single', (req, res) => {
+  const randomQuote = getRandomQuote();
+  res.json(randomQuote);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on PORT ${PORT}`);
+  console.log(`Quotes API service is running on port ${PORT}`);
 });
